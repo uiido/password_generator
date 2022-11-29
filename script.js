e9fd39ba04ab5e9b4af296089a63d2859b2cca1b
uiido <uiido@yahoo.com>
2022-11-28 19:42:01 -0500

    Filled out variables for characters.

---
+++ script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+    // YOUR CODE GOES HERE
+    var isUppercase = confirm('Would you like to use uppercase?');
+    var isLowercase = confirm('Would you like to use lowercase?');
+    var isSpecial = confirm('Would you like to use special characters?');
+    var isNumber = confirm('Would you like to use numbers?');
+}
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
