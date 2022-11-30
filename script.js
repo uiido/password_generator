662816950dd44d58bdc2280fd1af58f6670f7853
uiido <uiido@yahoo.com>
2022-11-30 00:26:37 -0500

    Tweaked if statements

--- script.js
+++ script.js
@@ -7,33 +7,36 @@ function generatePassword() {
     var numbers = "0123456789";
     var letters = "abcdefghijklmnopqrstuvwxyz";
     var upLetters = letters.toUpperCase();
-    var length = prompt('How many characters would you like your password to be?');
+    var characterLength = prompt('How many characters would you like your password to be?');
     var isUppercase = confirm('Would you like to use uppercase?');
     var isLowercase = confirm('Would you like to use lowercase?');
     var isSpecial = confirm('Would you like to use special characters?');
     var isNumber = confirm('Would you like to use numbers?');
 }
 
-//if (isUppercase) {
-//    // Use Uppercase Letters
-//    console.log('uppercase');
-//}
+console.log(Math.floor(Math.random() * 26));
 
-//if (isLowercase) {
-//    // Use Lowercase Letters
-//    console.log('lowercase');
-//}
+if (characterLength) {
+    //
+}
 
-//if (isSpecial) {
-//    // Use Special Characters
-//    console.log('special');
-//}
+if (isUppercase) {
+    //
+}
 
-//if (isNumber) {
-//    // Use Numbers
-//    console.log('numbers');
-//    alert('Click OK to confirm use of numbers.');
-//}
+if (isLowercase) {
+    //
+}
+
+if (isSpecial) {
+    //
+}
+
+if (isNumber) {
+    //
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
