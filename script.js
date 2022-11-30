7ab06b57e6d5e9410f174f47c75548c494f1bfcd
uiido <uiido@yahoo.com>
2022-11-30 00:49:10 -0500

    converted characters to arrays

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
     // YOUR CODE GOES HERE
-    var special = "#$%&'()*+,-./:;<=>?@[\]^_`{|}~!";
-    var numbers = "0123456789";
-    var letters = "abcdefghijklmnopqrstuvwxyz";
+    var special = ["#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "!"];
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var upLetters = letters.toUpperCase();
     var characterLength = prompt('How many characters would you like your password to be?');
     var isUppercase = confirm('Would you like to use uppercase?');
