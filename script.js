b302c4a30c48ca8359963dd60f543768685c72cb
uiido <uiido@yahoo.com>
2022-11-30 00:53:52 -0500

    Changed upper letters to array

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function generatePassword() {
     // YOUR CODE GOES HERE
     var special = ["#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "!"];
     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var upLetters = letters.toUpperCase();
+    var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var characterLength = prompt('How many characters would you like your password to be?');
     var isUppercase = confirm('Would you like to use uppercase?');
     var isLowercase = confirm('Would you like to use lowercase?');
@@ -14,27 +14,27 @@ function generatePassword() {
     var isNumber = confirm('Would you like to use numbers?');
 }
 
-console.log(Math.floor(Math.random() * 26));
+// console.log(Math.floor(Math.random() * 26));
 
-if (characterLength) {
-    //
-}
+//if (characterLength) {
+//
+//}
 
-if (isUppercase) {
-    //
-}
+//if (isUppercase) {
+//
+//}
 
-if (isLowercase) {
-    //
-}
+//if (isLowercase) {
+//
+//}
 
-if (isSpecial) {
-    //
-}
+//if (isSpecial) {
+//
+//}
 
-if (isNumber) {
-    //
-}
+//if (isNumber) {
+//
+//}
 
 
 
