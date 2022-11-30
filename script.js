96eec419042db2dc1a0814f501a91e82a9182aa1
uiido <uiido@yahoo.com>
2022-11-30 02:34:18 -0500

    Added parse for character length

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ function generatePassword() {
     var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    var characterLength = prompt('How many characters would you like your password to be?');
+    // Parse converts a string into a number
+    var characterLength = parseInt(prompt('How many characters would you like your password to be?'));
     var isUppercase = confirm('Would you like to use uppercase?');
     var isLowercase = confirm('Would you like to use lowercase?');
     var isSpecial = confirm('Would you like to use special characters?');
@@ -15,13 +16,13 @@ function generatePassword() {
     var passArray = [];
 }
 
-// If statements - selects values for each type
-if (isLowercase) {
-    passArray = passArray.concat(lowerLetters);
-}
-
+// If statements - selects values for each type + adds values to the password currently being generated
 if (isUppercase) {
     passArray = passArray.concat(upperLetters);
+}
+
+if (isLowercase) {
+    passArray = passArray.concat(lowerLetters);
 }
 
 if (isSpecial) {
